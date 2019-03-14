cec46eb8c162f1a0a456752fd8e6c2d98889afaa
Gerhard Serton <gerhardserton@gmail.com>
2019-03-14 08:13:23 +0200

    1st (seemingly) working iteration

--- driver.cpp
+++ driver.cpp
@@ -8,5 +8,32 @@ int main(int argc, char* argv[])
   VolImage image;
   string header = argv[1];
   image.readImages(header);
+  //cout << "Header: " << header << "\n";
+  if (argc == 2)
+  {
+    image.volImageSize();
+  }
+  else
+  {
+    string command = argv[2];
+    //cout << "Command: " << command << "\n";
+    if (command == "-d")
+    {
+      int sliceI = strtol(argv[3], NULL, 10);
+      int sliceJ = strtol(argv[4], NULL, 10);
+      //cout << "-d numbers: " << sliceI << " " << sliceJ << "\n";
+      string filename = argv[5];
+
+      image.diffmap(sliceI, sliceJ, filename);
+    }
+    else if (command == "-x")
+    {
+      int chosenSlice = strtol(argv[3], NULL, 10);
+      //cout << "-x numbers: " << chosenSlice << "\n";
+      string filename = argv[4];
+      image.extract(chosenSlice, filename);
+    }
+  }
+  //image.~VolImage();
   return 0;
 }
